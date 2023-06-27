ed0895d6016d7bcc5c831de0bff734104ee5d8dc
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-06-27 19:32:49 +0430

    ContextManager

--- ex.py
+++ ex.py
@@ -817,3 +817,25 @@
 # print(obj.__dict__)
 
 # -----------------------------------------------
+
+class FileManager:
+    def __init__(self, file_name, mode):
+        self.file_name = file_name
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        print('start')
+        self.file = open(self.file_name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('end')
+        self.file.close()
+
+
+with FileManager("file.txt", 'w') as f:
+    print('file created')
+    f.write('hi!')
+
+# -----------------------------------------------
